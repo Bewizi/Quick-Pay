351143732fbbcd7b03e391c8d8b5119702845e69
Bewizi <toluwalase.bewaji@yahoo.com>
2025-04-07 14:29:47 +0100

    Restructed header component for navbar, added navlinks,genbutton component i created

--- components/Header.vue
+++ components/Header.vue
@@ -1,31 +1,40 @@
-<script setup lang="ts">
-import Button from './ui/button/Button.vue'
-import { AlignCenterIcon } from 'lucide-vue-next';
-</script>
+<script lang="ts" setup></script>
 
 <template>
-    <nav class="flex justify-between items-center border-2 border-red">
-        <a href="#">
-            <!-- <NuxtImg
-            src="/public/images/rovel-Logo.svg"
-            alt="image file description"
-            /> -->
-            <img src="/public/images/rovel-Logo.svg" alt="rovel sub" width="45px">
-        </a>
-        <ul class="flex">
-            <li>Home</li>
-            <li>Features</li>
-            <li>About Us</li>
-            <li>How it works</li>
-            <li>FAQS</li>
-            <li>Contact Us</li>
+  <section class="container max-w-7xl mx-auto px-8 py-4 lg:px-5">
+    <header class="flex justify-between items-center">
+      <a href="#">
+        <!-- <NuxtImg
+        src="/public/images/rovel-Logo.svg"
+        alt="image file description"
+        /> -->
+        <img alt="rovel sub" src="/public/images/rovel-Logo.svg" width="45px"/>
+      </a>
+      <nav class="">
+        <ul class="flex items-center space-x-8">
+          <NuxtLink to="/">Home</NuxtLink>
+          <NuxtLink to="#">Features</NuxtLink>
+          <NuxtLink to="#">About Us</NuxtLink>
+          <NuxtLink to="#">How it works</NuxtLink>
+          <NuxtLink to="#">FAQS</NuxtLink>
+          <NuxtLink to="">Contact Us</NuxtLink>
         </ul>
-        <div>
-            <Button>Login</Button>
-            <Button>Register</Button>
-        </div>
-        <Button>
-            <AlignCenterIcon />
-        </Button>
-    </nav>
+      </nav>
+
+      <!--      REGISTRATION -->
+      <div class="flex items-center space-x-4">
+        <GenButton
+            default-class="border-2 border-blue-500 bg-transparent text-[#0045A5] hover:bg-transparent rounded-full px-8">
+          Login
+        </GenButton>
+        <GenButton default-class="">Register</GenButton>
+        <!--        <GenButton defaultClass="">Bus</GenButton>-->
+      </div>
+
+      <!--      FOR MOBILE SCREEN MENU BAR AND CLOSE -->
+      <!--      <Button>-->
+      <!--        <AlignCenterIcon/>-->
+      <!--      </Button>-->
+    </header>
+  </section>
 </template>
