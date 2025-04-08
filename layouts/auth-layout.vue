75431a4f945cc02444805a4d5d352ba34e313f36
Bewizi <toluwalase.bewaji@yahoo.com>
2025-04-08 14:08:48 +0100

    Created an auth layout for login and signup page

--- layouts/auth-layout.vue
+++ layouts/auth-layout.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 
 </script>
 
 <template>
-  $END$
+  <section>
+    <div class="flex flex-col items-center justify-center min-h-screen py-2">
+      <div class="w-full max-w-sm p-8 space-y-3 rounded-xl shadow-md bg-gray-50 text-gray-800">
+        <h1 class="text-2xl font-bold text-center">Welcome to our App</h1>
+        <slot/>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
