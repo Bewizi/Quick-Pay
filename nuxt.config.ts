23b7a5ca256a87b5c5f37007c52aad0f58851208
Bewizi <toluwalase.bewaji@yahoo.com>
2025-04-08 14:10:19 +0100

    added an app object, containing title,meta and link for seo optimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-04-05',
+  app: {
+    head: {
+      title: "Quick Pay",
+      meta: [
+        { name: "description", content: "Quick Pay application" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+  compatibilityDate: "2025-04-05",
   devtools: { enabled: true },
-  css:['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   image: {
-    dir: 'public/images'
+    dir: "public/images",
   },
   postcss: {
     plugins: {
@@ -12,5 +22,5 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image']
-})
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/image"],
+});
